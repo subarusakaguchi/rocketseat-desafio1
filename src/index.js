61a5eb67f61ff2905e2184f3fb60a19e9d9fffa5
Subaru Sakaguchi <subarusakaguchi@yahoo.com.br>
2021-10-28 23:30:24 -0300

    Feito post/users

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid')
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = req.body
+  const newUser = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  }
+
+  users.push(newUser)
+
+  return response.status(201).send(newUser)
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
